e7a494261a4c215badd4c306deab41a8a4913abb
AnthonioFuentes09 <dafuentes@intermoda.com.hn>
2026-02-12 23:12:54 -0600

    2026-02-12 | Implementacion ejercicio 3 Se integro el ejercicio #3 donde utilizamos un boton donde podemos cambiar el estado de una variable llamada mostrarDetalle, la cual nos ayuda a mostrar una tarjeta de informacion, y una imagen, asi como a ocultarlo.

--- Tarea1Semana3/App.tsx
+++ Tarea1Semana3/App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Ejercicio1 from './src/components/ejercicio1';
 import Ejercicio2 from './src/components/ejercicio2';
 import Ejercicio3 from './src/components/ejercicio3';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.card}>
-        <Text style={{color: '#000000', fontSize: 30, fontWeight: 'bold'}}>Tarea 1 - Semana 3</Text>
-        <StatusBar style="auto" />
-        <Ejercicio1 />
-        <Ejercicio2 />
-        <Ejercicio3 />
+    <ScrollView >
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <Text style={{color: '#000000', fontSize: 30, fontWeight: 'bold'}}>Tarea 1 - Semana 3</Text>
+          <StatusBar style="auto" />
+          <Ejercicio1 />
+          <Ejercicio2 />
+          <Ejercicio3 />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
--- Tarea1Semana3/src/components/ejercicio3.tsx
+++ Tarea1Semana3/src/components/ejercicio3.tsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
 
 export default function Ejercicio3() {
-  return (
-    <View style={styles.container}>
-        <View style={styles.form}>
-            <Text style={styles.text}>Ejercicio 3</Text>
+    const [mostrarDetalles, setMostrarDetalles] = useState(false);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.form}>
+                <Text style={styles.text}>Ejercicio 3 - Toggle de visibilidad</Text>
+                
+                <TouchableOpacity 
+                    style={styles.boton}
+                    onPress={() => setMostrarDetalles(!mostrarDetalles)}
+                >
+                    <Text style={styles.buttonText}>{mostrarDetalles ? "Ocultar detalles" : "Mostrar detalles"}</Text>
+                    
+                </TouchableOpacity>
+
+                { mostrarDetalles && (
+                    <View style={styles.detalles}>
+                        <Text style={styles.subText}>Detalles del usuario Altair.</Text>
+                        <Image
+                            source={require('../../assets/1366_2000.jpeg')}
+                            style={styles.imagen}
+                        />
+                    </View>
+                )}
+                {
+                    !mostrarDetalles && <Text style={styles.subText}>Detalles ocultos.</Text>
+                }
+
+            </View>
         </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
@@ -20,6 +45,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#f6f6f6',
         alignItems: 'center',
         marginTop: 20,
+        marginBottom: 40,
     },
     form: {
         width: "90%",
@@ -27,5 +53,30 @@ const styles = StyleSheet.create({
     text: {
         color: '#000000',
         fontSize: 20,
+    },
+    subText: {
+        color: '#000000',
+        fontSize: 16,
+        marginTop: 10,
+    },
+    boton: {    
+        backgroundColor: '#007bff',
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+        alignItems: 'center',
+    },
+    detalles: {
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
+    },
+    imagen: {
+        width: 200,
+        height: 200,
+        marginTop: 10,
     },
 });
